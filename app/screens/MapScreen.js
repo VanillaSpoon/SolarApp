20316ae43cc27716d9bd439d874ed35f86e79385
Eoin Gallinagh <eoingallinagh@gmail.com>
2022-02-15 20:59:33 +0100

    add ability to calculate area between 4 points

--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { View, TextInput, Button } from "react-native";
+import { View, TextInput, Button, Text } from "react-native";
 import Screen from "../components/Screen";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import Geocoder from "react-native-geocoding";
+import haversine from "haversine";
 
 function MapScreen() {
   const [searchText, setSearchText] = useState("");
@@ -12,6 +13,8 @@ function MapScreen() {
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   });
+  const [markers, setMarkers] = useState([]);
+  const [area, setArea] = useState(null);
 
   useEffect(() => {
     Geocoder.init("AIzaSyBIcIDCgseIHKADEEOzCrfV1ku927QlpV4");
@@ -35,6 +38,32 @@ function MapScreen() {
       .catch((error) => console.warn(error));
   };
 
+  const handleMapPress = (event) => {
+    const newMarkers = [...markers, event.nativeEvent.coordinate];
+    setMarkers(newMarkers);
+  };
+
+  const handleCalculateArea = () => {
+    if (markers.length !== 4) {
+      alert("Please select 4 points on the map");
+      return;
+    }
+
+    const [point1, point2, point3, point4] = markers;
+    const distance1 = haversine(point1, point2, { unit: "meter" });
+    const distance2 = haversine(point2, point3, { unit: "meter" });
+    const distance3 = haversine(point3, point4, { unit: "meter" });
+    const distance4 = haversine(point4, point1, { unit: "meter" });
+    const semiperimeter = (distance1 + distance2 + distance3 + distance4) / 2;
+    const area = Math.sqrt(
+      (semiperimeter - distance1) *
+        (semiperimeter - distance2) *
+        (semiperimeter - distance3) *
+        (semiperimeter - distance4)
+    );
+    setArea(area.toFixed(2));
+  };
+
   return (
     <Screen>
       <View style={{ flexDirection: "row", padding: 10 }}>
@@ -51,7 +80,16 @@ function MapScreen() {
         mapType="hybrid"
         region={region}
         showsUserLocation={true}
-      />
+        onPress={handleMapPress}
+      >
+        {markers.map((marker, index) => (
+          <Marker key={index} coordinate={marker} />
+        ))}
+      </MapView>
+      <Button title="Calculate Area" onPress={handleCalculateArea} />
+      {area && (
+        <Text style={{ alignSelf: "center" }}>Area: {area} square meters</Text>
+      )}
     </Screen>
   );
 }
