74c72b530e62347244ac64862c8183421d95b309
Eoin Gallinagh <eoingallinagh@gmail.com>
2022-10-24 18:11:35 +0100

    add: Login & Register

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Screen from "./app/components/Screen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import RegisterScreen from "./app/screens/RegisterScreen";
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default class App extends React.Component {
   render() {
     return (
       <Screen>
-        <WelcomeScreen />
+        <RegisterScreen />
       </Screen>
     );
   }
---
+++ app/screens/RegisterScreen.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { StyleSheet, TextInput, View, Text } from "react-native";
+import Screen from "../components/Screen";
+import Button from "../components/Button";
+
+function RegisterScreen() {
+  return (
+    <Screen style={styles.container}>
+      <Text style={styles.tagline}>Solar</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="E-mail"
+        keyboardType="email-address"
+        autoCompleteType="off"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+      />
+      <View style={styles.buttons}>
+        <Button title="Register" color="primary" />
+      </View>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonsContainer: {
+    padding: 20,
+    width: "100%",
+  },
+  input: {
+    width: 300,
+    borderRadius: 6,
+    borderWidth: 2,
+    borderColor: "#6d69c3",
+    marginVertical: 10,
+    padding: 15,
+    alignSelf: "center",
+  },
+  container: {
+    padding: 10,
+  },
+  tagline: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 20,
+    alignSelf: "center",
+  },
+});
+
+export default RegisterScreen;
